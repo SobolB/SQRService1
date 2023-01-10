1dff2c9163415e1d699d731e55862132bdda9c1f
SobolB <79850869+SobolB@users.noreply.github.com>
2023-01-11 01:08:39 +0300

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,13 +1,13 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int calcul(int start,int end){
+    public int calcul(int start, int end) {
         int count = 0;
-        for(int i=10; i<=99; i++){
-            int cvadr=i*i;
-            if(cvadr >= start){
-                if(cvadr <= end) {
-                    count ++;
+        for (int i = 10; i <= 99; i++) {
+            int cvadr = i * i;
+            if (cvadr >= start) {
+                if (cvadr <= end) {
+                    count++;
                 }
             }
         }
